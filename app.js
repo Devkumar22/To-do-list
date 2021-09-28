97504107bb6ec645c7a2eb00d67cbf03633912cb
Dev kumar <55612982+Devkumar22@users.noreply.github.com>
2021-09-28 11:04:31 +0530

    ejs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //jshint esversion:6
 
 const express = require("express");
@@ -23,3 +24,37 @@ app.get("/", function(req, res){
 app.listen(3000, function(){
     console.log("serrver started on port 3000");
 });
+=======
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+
+    var today=new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+ 
+    var day = today.toLocaleDateString("en-US", options);
+
+    
+    res.render("list", {
+        kindofDay: day
+    });
+
+});
+
+
+app.listen(3000, function(){
+    console.log("serrver started on port 3000");
+});
+>>>>>>> Stashed changes
